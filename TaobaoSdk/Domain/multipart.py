31428c4fdd7481d314d7920073cd7f8314b9dd96
xieguanfu <xieguanfu@maimiaotech.com>
2014-06-17 11:25:50 +0800

    添加图片上传

--- TaobaoSdk/Domain/multipart.py
+++ TaobaoSdk/Domain/multipart.py
@@ -286,3 +286,19 @@ class ItemImgUploadRequest(RestApi):
 
 	def getMultipartParas(self):
 		return ['image']
+
+class PictureUploadRequest(RestApi):
+	def __init__(self,domain='gw.api.taobao.com',port=80):
+		RestApi.__init__(self,domain, port)
+		self.picture_category_id = None
+		self.image = None
+		self.img = None
+		self.title = None
+		self.client_type = None
+		self.image_input_title = None
+
+	def getapiname(self):
+		return 'taobao.picture.upload'
+
+	def getMultipartParas(self):
+		return ['img']
